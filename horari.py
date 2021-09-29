93a2c5c330ca8132bbc7832e7ea3d8e28660d93a
nullptr <eliesbertran@gmail.com>
2021-09-29 23:53:06 +0200

    Huge refactor

--- horari.py
+++ horari.py
@@ -14,6 +14,9 @@ from svglib.svglib import svg2rlg, register_font
 from reportlab.graphics import renderPDF
 import datetime
 
+from typing import List, Dict, Union
+
+
 class SubjectProps:
 	def __init__(self, name: str, color: str):
 		self.name = name
@@ -37,6 +40,15 @@ SUBJECT_PROPS = {
 	'100099': SubjectProps('TM',  '#ffb3ad'),
 	'100087': SubjectProps('FVR', '#aba6ff'),
 }
+
+# SUBJECT_PROPS = {
+# 	# '100095': SubjectProps('GL',  '#ffd28a'),
+# 	'100088': SubjectProps('ALG', '#afeeee'),
+# 	'100090': SubjectProps('FIS',  '#ffd28a'),
+# 	# '100090': SubjectProps('FIS',  '#e9ffa4'),
+# 	# '100099': SubjectProps('TM',  '#ffb3ad'),
+# 	'100087': SubjectProps('FVR', '#aba6ff'),
+# }
 
 # Courses to get the data from
 courses = ['CURS - 1', 'CURS - 2']
@@ -102,6 +114,7 @@ for result in results:
 
 driver.close()
 
+
 #####################################################
 # Parse the data collected from the website into a
 # manejable format for drawing and draw it to a svg
@@ -125,11 +138,11 @@ class HolidayType(IntEnum):
 
 # Stores information of a subject
 class Subject:
-	def __init__(self, id: str, group: str, type: SubjectType, classroom: str):
-		self.id = id
-		self.group = group
-		self.type = type
-		self.classroom = classroom
+	def __init__(self):
+		self.id: str = None
+		self.group: str = None
+		self.type: SubjectType = None
+		self.classroom: str = None
 	
 	def __str__(self):
 		return f"{SUBJECT_PROPS[self.id].name}: {self.group} - {self.type} at {self.classroom}"
@@ -137,6 +150,38 @@ class Subject:
 	def __repr__(self):
 		return f"Subject({self})"
 
+
+class Day:
+	def __init__(self):
+		self.hours: Dict[int, List[Subject]] = dict() # key = hour, value = ubject
+		self.holiday: Union[None, HolidayType] = None
+		self.date: datetime.date = None
+		
+	def add_subject(self, hour, subject):
+		if hour not in self.hours.keys():
+			self.hours[hour] = list()
+		
+		self.hours[hour].append(subject)
+	
+	def __str__(self):
+		return f"holiday={self.holiday} {self.hours}"
+
+class Week:
+	def __init__(self, date: datetime.date):
+		self.days = {
+			0: Day(), # Monday
+			1: Day(), # Tuesday
+			2: Day(), # Wednesday
+			3: Day(), # Thursday
+			4: Day(), # Friday
+		}
+		for day_n, day in self.days.items():
+			day.date = date + datetime.timedelta(days=day_n)
+	
+	def __getitem__(self, day):
+		assert 0 <= day < 5, "Day must be a number between 0 and 4"
+		return self.days[day]
+		
 
 # Get the screen position from an event and output its day
 def get_day_from_event(event):
@@ -177,25 +222,13 @@ MONTHS = [
 
 register_font('calibri', 'calibri_bold.ttf', 'bold')
 
-date = datetime.date(starting_year, starting_month, starting_day)
+# Will store all weeks
+weeks: List[Week] = list()
 
+date = datetime.date(starting_year, starting_month, starting_day)
 for week_n, result in enumerate(results):
 	
-	#############################################
-	# Make a table of every subject of the week
-	#############################################
-	
-	# Stores which subjects occurred in hour of each day
-	table = {
-		0: dict(), # Monday
-		1: dict(), # Tuesday
-		2: dict(), # Wednesday
-		3: dict(), # Thursday
-		4: dict(), # Friday
-	}
-	
-	# Which days of the week are holidays (no lectiu o festiu)
-	holidays = dict()
+	week = Week(date)
 	
 	parsed = BeautifulSoup(result, 'html.parser')
 	
@@ -204,102 +237,109 @@ for week_n, result in enumerate(results):
 	for event in parsed.select('div[class*="fc-event fc-event-vert fc-event-start fc-event-end"]'):
 		
 		content = event.find('div', {'class': 'fc-event-title'})
+		day_n = get_day_from_event(event)
 		
-		# Check if it's holiday (then skip everything else)
+		# Check if it's holiday
 		if content.getText() == "Dia festiu":
-			day = get_day_from_event(event)
-			holidays[day] = HolidayType.FESTIU
-			continue
+			week[day_n].holiday = HolidayType.FESTIU
 	
 		elif content.getText() ==  "Dia no lectiu":
-			day = get_day_from_event(event)
-			holidays[day] = HolidayType.NO_LECTIU
-			continue
+			week[day_n].holiday = HolidayType.NO_LECTIU
 		
-		
-		content = str(content.find('p'))
-		subject_id = re.search('(?<=<p>).*?(?= -)', content).group(0)
-		
-		if subject_id in SUBJECT_PROPS.keys():
+		else:
+			content = str(content.find('p'))
+			subject_id = re.search('(?<=<p>).*?(?= -)', content).group(0)
 			
-			group_text = re.search('(?<=Grup ).*?(?=<br/>)', content).group(0)
-			
-			group = group_text[:group_text.find(' - ')]
-			type_text = group_text[group_text.find(' - ')+3:]
-			
-			subject_type = None
-			if type_text == 'Teoria':
-				subject_type = SubjectType.THEORY
-			elif type_text == "Pràctiques d'Aula":
-				subject_type = SubjectType.PROBLEMS
-			elif type_text == "Pràctiques de Laboratori":
-				subject_type = SubjectType.LABORATORY
-			elif type_text == "Seminaris":
-				subject_type = SubjectType.SEMINAR
-			elif type_text == "Examen":
-				subject_type = SubjectType.EXAM
-			else:
-				subject_type = SubjectType.UNKNOWN
-				print(f"ERROR: {event} has not valid type '{type_text}'")
-			
-			
-			hours = str(event.find('div', {'class': 'fc-event-time'}).contents[0])
-			
-			start_time = int(hours[:2])
-			end_time = int(hours[8:10])
-			
-			classroom = re.search('(?<=Aula ).*?(?= -)', content)
-			if classroom is not None:
-				classroom = classroom.group(0)
-			else:
-				classroom = ''
-			
-			day = get_day_from_event(event)
-			
-			for h in range(start_time, end_time):
-				if h not in table[day].keys():
-					table[day][h] = list()
+			if subject_id in SUBJECT_PROPS.keys():
 				
-				table[day][h].append(Subject(subject_id, group, subject_type, classroom))
+				subject = Subject()
+				
+				subject.id = subject_id
+				
+				# Get the group
+				group_text = re.search('(?<=Grup ).*?(?=<br/>)', content).group(0)
+				subject.group = group_text[:group_text.find(' - ')]
+				
+				# Get the subject type
+				type_text = group_text[group_text.find(' - ')+3:]
+				if type_text == 'Teoria':
+					subject.type = SubjectType.THEORY
+				elif type_text == "Pràctiques d'Aula":
+					subject.type = SubjectType.PROBLEMS
+				elif type_text == "Pràctiques de Laboratori":
+					subject.type = SubjectType.LABORATORY
+				elif type_text == "Seminaris":
+					subject.type = SubjectType.SEMINAR
+				elif type_text == "Examen":
+					subject.type = SubjectType.EXAM
+				else:
+					subject.type = SubjectType.UNKNOWN
+					print(f"ERROR: {event} has not valid type '{type_text}'")
+				
+				# Get the classroom
+				classroom = re.search('(?<=Aula ).*?(?= -)', content)
+				if classroom is not None:
+					subject.classroom = classroom.group(0)
+				else:
+					subject.classroom = ''
+			
+				# Get the hour
+				hours = str(event.find('div', {'class': 'fc-event-time'}).contents[0])
+				
+				start_time = int(hours[:2])
+				end_time = int(hours[8:10])
+				
+				# Add the Subject
+				for h in range(start_time, end_time):
+					
+					week[day_n].add_subject(h, subject)
 	
+	weeks.append(week)
+	date += datetime.timedelta(weeks=1)
 	
-	#########################################
-	# Draw the collected data into a svg
-	#########################################
-	
-	n_hours = 11 # From 9 to 20
-	first_hour = 9
-	
-	width = 1000.0
-	height = 500.0
-	
-	hour_label_width = width * 0.05
-	day_label_height = height / (n_hours + 1)
-	
-	work_width = width - hour_label_width
-	work_height = height - day_label_height
-	day_width = work_width / 5
-	day_height = work_height / n_hours
-	
-	text_size = day_height * 0.3
-	
-	light_line_width = height / 1000
-	dark_line_width = 2 * light_line_width
+
+#########################################
+# Draw the collected data into a svg
+#########################################
+
+n_hours = 11 # From 9 to 20
+first_hour = 9
+
+width = 1000.0
+height = 500.0
+
+hour_label_width = width * 0.05
+day_label_height = height / (n_hours + 1)
+
+work_width = width - hour_label_width
+work_height = height - day_label_height
+day_width = work_width / 5
+day_height = work_height / n_hours
+
+text_size = day_height * 0.3
+
+light_line_width = height / 1000
+dark_line_width = 2 * light_line_width
+
+def get_day_x(day):
+	return hour_label_width + day_width * day
+
+def get_hour_y(hour):
+	return height - day_label_height - day_height - (hour - first_hour) * day_height
+
+for week_n, week in enumerate(weeks):
 	
 	d = draw.Drawing(width, height)
 	
 	d.append(draw.Rectangle(0, 0, width, height, fill='white', stroke='black', stroke_width=dark_line_width)) # Background
 	
-	def get_day_x(day):
-		return hour_label_width + day_width * day
-	
 	# For each day get the subjects (data)
-	for day, data in enumerate(table.values()):
+	for day_n, day in week.days.items():
 		
-		day_x = get_day_x(day)
+		day_x = get_day_x(day_n)
 		
 		# Draw gray background on the day label
-		if day in holidays.keys():
+		if day.holiday is not None:
 			d.append(draw.Rectangle(
 				day_x,
 				height - day_label_height,
@@ -310,84 +350,52 @@ for week_n, result in enumerate(results):
 				stroke_width='0.2'
 			))
 		
-		# Draw day and month text on top
-		d.append(draw.Text(
-			f"{date.day}-{MONTHS[date.month-1]}",
-			text_size,
-			day_x + day_width / 2,
-			height - day_label_height / 2,
-			center=True,
-			fill='black',
-			font_weight='bold',
-			font_family='calibri'
-		))
-		
-		date += datetime.timedelta(days=1)
-		
-		for hour in range(first_hour, first_hour + n_hours):
+		for hour, subjects in day.hours.items():
+			y = get_hour_y(hour)
 			
-			y = height - day_label_height - day_height - (hour - first_hour) * day_height
-			
-			# Draw hours (only one time)
-			if day == 0:
+			s: Subject
+			for i, s in enumerate(subjects):
+				
+				new_width = day_width / len(subjects)
+				
+				x = day_x + i * new_width
+				
+				props = SUBJECT_PROPS[s.id]
+				
+				text = props.name
+				if s.type == SubjectType.SEMINAR:
+					text += f" Se{s.group}"
+				elif s.type == SubjectType.PROBLEMS:
+					text += f" Pb{s.group}"
+				elif s.type == SubjectType.LABORATORY:
+					text += f" Pr{s.group}"
+				elif s.type == SubjectType.EXAM:
+					text += f" {s.group} Examen"
+				elif s.type == SubjectType.UNKNOWN:
+					text += f" {s.group} (??)"
+				
+				text += f"\n{s.classroom}"
+				
+				d.append(draw.Rectangle(
+					x,
+					y,
+					new_width,
+					day_height,
+					fill=props.color,
+					stroke='black',
+					stroke_width=dark_line_width
+				))
+				
 				d.append(draw.Text(
-					f"{hour}-{hour+1}",
+					text,
 					text_size,
-					hour_label_width / 2,
+					x + new_width / 2,
 					y + day_height / 2,
 					center=True,
 					fill='black',
 					font_weight='bold',
 					font_family='calibri'
 				))
-			
-			if hour in data.keys():  # If there are any subjects in the hour
-				subjects = data[hour]
-				
-				s: Subject
-				for i, s in enumerate(subjects):
-					
-					new_width = day_width / len(subjects)
-					
-					x = day_x + i * new_width
-					
-					props = SUBJECT_PROPS[s.id]
-					
-					text = props.name
-					if s.type == SubjectType.SEMINAR:
-						text += f" Se{s.group}"
-					elif s.type == SubjectType.PROBLEMS:
-						text += f" Pb{s.group}"
-					elif s.type == SubjectType.LABORATORY:
-						text += f" Pr{s.group}"
-					elif s.type == SubjectType.EXAM:
-						text += f" {s.group} Examen"
-					elif s.type == SubjectType.UNKNOWN:
-						text += f" {s.group} (??)"
-					
-					text += f"\n{s.classroom}"
-					
-					d.append(draw.Rectangle(
-						x,
-						y,
-						new_width,
-						day_height,
-						fill=props.color,
-						stroke='black',
-						stroke_width=dark_line_width
-					))
-					
-					d.append(draw.Text(
-						text,
-						text_size,
-						x + new_width / 2,
-						y + day_height / 2,
-						center=True,
-						fill='black',
-						font_weight='bold',
-						font_family='calibri'
-					))
-	
 	
 	# Draw grid
 	for i in range(1, 5):
@@ -401,46 +409,72 @@ for week_n, result in enumerate(results):
 	d.append(draw.Line(0, height - day_label_height, width, height - day_label_height, stroke='black', stroke_width=dark_line_width))
 	d.append(draw.Line(hour_label_width, 0, hour_label_width, height, stroke='black', stroke_width=dark_line_width))
 	
-	# Draw holidays as gray
-	for day in holidays.keys():
-		day_x = get_day_x(day)
-		d.append(draw.Rectangle(
-			day_x,
-			0,
-			day_width,
-			work_height,
-			fill='#BEBEBE',
-			stroke='black',
-			stroke_width='0.2'
-		))
-		
-		holiday_text = None
-		if holidays[day] == HolidayType.FESTIU:
-			holiday_text = 'FESTIU'
-		elif holidays[day] == HolidayType.NO_LECTIU:
-			holiday_text = 'NO LECTIU'
-		else:
-			holiday_text = 'FESTA??'
-		
+	# Draw day and month text on top
+	for day_n, day in week.days.items():
+		day_x = get_day_x(day_n)
 		d.append(draw.Text(
-			holiday_text,
+			f"{day.date.day}-{MONTHS[day.date.month-1]}",
 			text_size,
 			day_x + day_width / 2,
-			work_height / 2,
+			height - day_label_height / 2,
+			center=True,
+			fill='black',
+			font_weight='bold',
+			font_family='calibri'
+		))
+		
+	# Draw hours (only one time)
+	for hour in range(first_hour, first_hour + n_hours):
+		day_y = get_hour_y(hour)
+		d.append(draw.Text(
+			f"{hour}-{hour+1}",
+			text_size,
+			hour_label_width / 2,
+			day_y + day_height / 2,
 			center=True,
 			fill='black',
 			font_weight='bold',
 			font_family='calibri'
 		))
 	
+	# Draw holidays as gray
+	for day_n, day in week.days.items():
+		if day.holiday is not None:
+			day_x = get_day_x(day_n)
+			d.append(draw.Rectangle(
+				day_x,
+				0,
+				day_width,
+				work_height,
+				fill='#BEBEBE',
+				stroke='black',
+				stroke_width='0.2'
+			))
+	
+			holiday_text = None
+			if day.holiday == HolidayType.FESTIU:
+				holiday_text = 'FESTIU'
+			elif day.holiday == HolidayType.NO_LECTIU:
+				holiday_text = 'NO LECTIU'
+			else:
+				holiday_text = 'FESTA??'
+	
+			d.append(draw.Text(
+				holiday_text,
+				text_size,
+				day_x + day_width / 2,
+				work_height / 2,
+				center=True,
+				fill='black',
+				font_weight='bold',
+				font_family='calibri'
+			))
+	
 	with open(f'temp/{week_n}.svg', 'w', encoding='utf8') as file:
 		d.asSvg(outputFile=file)
 	
 	drawing = svg2rlg(f'temp/{week_n}.svg')
 	renderPDF.drawToFile(drawing, f'results/{week_n}.pdf')
-	
-	# Skip the weekend
-	date += datetime.timedelta(days=2)
 
 
 # Merge all pdfs into one
